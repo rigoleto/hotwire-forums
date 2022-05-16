2a33fbae0de227fc1098e3aec9fb4dc11eed012b
Reid Lynch <reid.lynch@gmail.com>
2022-05-16 15:53:55 -0400

    install action_text and active_storage

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,7 +2,7 @@ class DiscussionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @discussions = Discussion.all
+    @discussions = Discussion.all.order(updated_at: :desc)
   end
 
   def show
@@ -17,6 +17,7 @@ class DiscussionsController < ApplicationController
     @discussion = Discussion.new(discussion_params.merge(user: Current.user))
     respond_to do |format|
       if @discussion.save
+        @discussion.broadcast_prepend_to("discussions")
         format.html { redirect_to @discussion, notice: "Discussion created" }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -30,9 +31,9 @@ class DiscussionsController < ApplicationController
 
   def update
     @discussion = Discussion.find params[:id]
-    @discussion.assign_attributes discussion_params
     respond_to do |format|
-      if @discussion.save
+      if @discussion.update(discussion_params)
+        @discussion.broadcast_replace_to("discussions")
         format.html { redirect_to @discussion, notice: "Discussion updated" }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,6 +44,7 @@ class DiscussionsController < ApplicationController
   def destroy
     @discussion = Discussion.find params[:id]
     @discussion.destroy!
+    @discussion.broadcast_remove_to("discussions")
     redirect_to discussions_path, notice: "Discussion removed"
   end
 
