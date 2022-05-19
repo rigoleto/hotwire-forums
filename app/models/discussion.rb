5c03b0183cda64e1e52e8f78a66b715f91332e10
Reid Lynch <reid.lynch@gmail.com>
2022-05-19 14:45:37 -0400

    discussion subscription methods

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,6 +2,10 @@ class Discussion < ApplicationRecord
   belongs_to :user
   belongs_to :category, touch: true, optional: true
   has_many :posts, dependent: :destroy
+  has_many :users, through: :posts
+  has_many :discussion_subscriptions, dependent: :destroy
+  has_many :subscribers, -> { merge(DiscussionSubscription.opt_in) }, through: :discussion_subscriptions, source: :user
+  has_many :ignorers, -> { merge(DiscussionSubscription.opt_out) }, through: :discussion_subscriptions, source: :user
 
   accepts_nested_attributes_for :posts, reject_if: proc {|attrs| attrs[:body].blank? }
 
@@ -16,5 +20,22 @@ class Discussion < ApplicationRecord
 
   def to_param
     "#{id}-#{name.downcase}"[0..100].parameterize
+  end
+
+  def subscribed_users
+    (users + subscribers) - ignorers
+  end
+
+  def subscription_for(user)
+    return nil if user.nil?
+    discussion_subscriptions.find_by(user: user)
+  end
+
+  def toggle_subscription!(user)
+    if subscription = subscription_for(user)
+      subscription.toggle!
+    elsif posts.where(user: user).any?
+      discussion_subscriptions.opt_out.where(user: user).create!
+    end
   end
 end
