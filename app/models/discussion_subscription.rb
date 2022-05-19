fd272f81a382b939b4b4ad3ab94e81974361fabf
Reid Lynch <reid.lynch@gmail.com>
2022-05-19 14:33:08 -0400

    subscription model

---
+++ app/models/discussion_subscription.rb
@@ -0,0 +1,14 @@
+class DiscussionSubscription < ApplicationRecord
+  belongs_to :discussion
+  belongs_to :user
+
+  scope :opt_in, -> { where(subscription_type: "opt_in") }
+  scope :opt_out, -> { where(subscription_type: "opt_out") }
+
+  validates :subscription_type, inclusion: { in: %w[opt_in opt_out] }
+  validates :user_id, uniqueness: { scope: :discussion_id }
+
+  def toggle!
+    update!(subscription_type: subscription_type == "opt_in" ? "opt_out" : "opt_in")
+  end
+end
