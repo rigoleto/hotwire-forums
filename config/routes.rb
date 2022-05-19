c8647fa6d0a162fe43d7d220afd87ebc7c9c911f
Reid Lynch <reid.lynch@gmail.com>
2022-05-19 15:15:42 -0400

    update subscription status from UI

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     collection do
       get "category/:id", to: "categories/discussions#index", as: :category
     end
+    resources :notifications, module: :discussions
   end
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
